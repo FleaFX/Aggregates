c632e02e7ec7fa3ebf9b7fee5ab038120548867a
FleaFX <wouter.naessens@outlook.com>
2023-11-23 10:40:48 +0100

    Make MetadataScope publicly accessible.

--- src/Aggregates/Metadata/MetadataScope.cs
+++ src/Aggregates/Metadata/MetadataScope.cs
@@ -13,13 +13,18 @@ public sealed class MetadataScope : IAsyncDisposable {
     /// <summary>
     /// Initializes a new <see cref="MetadataScope"/>.
     /// </summary>
-    internal MetadataScope(bool noPush = false) {
+    internal MetadataScope(bool noPush) {
         // attempt to copy values from outer scopes
         _metadata = (Scopes.TryPeek()?._metadata).CopyOrEmpty();
 
         if (!noPush)
             Scopes = Scopes.Push(this);
     }
+
+    /// <summary>
+    /// Initializes a new <see cref="MetadataScope"/>.
+    /// </summary>
+    public MetadataScope() : this(false) { }
 
     /// <summary>
     /// Gets the current <see cref="MetadataScope"/>.
@@ -33,6 +38,14 @@ public sealed class MetadataScope : IAsyncDisposable {
     /// <param name="metadata">The metadata to add.</param>
     internal void Add(KeyValuePair<string, object?> metadata) =>
         _metadata[metadata.Key] = metadata.Value;
+
+    /// <summary>
+    /// Adds the given metadata to the scope.
+    /// </summary>
+    /// <param name="key">The key by which the metadata will be referable.</param>
+    /// <param name="value">The value of the metadata.</param>
+    public void Add(string key, object? value) =>
+        Add(new KeyValuePair<string, object?>(key, value));
 
     /// <summary>
     /// Returns the metadata in the current scope as a <see cref="IDictionary{TKey,TValue}"/>.
