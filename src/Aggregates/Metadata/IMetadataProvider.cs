1c42656784216fa63ad9c2be134e7f736f48cb04
FleaFX <wouter.naessens@outlook.com>
2024-04-30 14:04:07 +0200

    Add GetValueAsync method to IMetadataProvider

--- src/Aggregates/Metadata/IMetadataProvider.cs
+++ src/Aggregates/Metadata/IMetadataProvider.cs
@@ -1,15 +1,27 @@
-﻿namespace Aggregates.Metadata;
+﻿using Aggregates.Util;
+
+namespace Aggregates.Metadata;
 
 /// <summary>
 /// Provides a value that should be added as metadata to a written event.
 /// </summary>
 /// <typeparam name="TContext">The type of the context that may provide more information to generate the metadata value.</typeparam>
 /// <typeparam name="TValue">The type of the metadata value.</typeparam>
-public interface IMetadataProvider<in TContext, out TValue> {
+public interface IMetadataProvider<in TContext, TValue> {
     /// <summary>
     /// Gets the value for a metadata
     /// </summary>
     /// <param name="context">The context that may provide more information to generate the metadata value.</param>
     /// <returns>A <typeparamref name="TValue"/>.</returns>
-    TValue GetValue(TContext context);
+    TValue GetValue(TContext context) =>
+        GetValueAsync(context, CancellationToken.None).RunSynchronously();
+
+    /// <summary>
+    /// Asynchronously gets the value for a metadata
+    /// </summary>
+    /// <param name="context">The context that may provide more information to generate the metadata value.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken" /> to cancel the asynchronous operation.</param>
+    /// <returns>A <typeparamref name="TValue"/>.</returns>
+    ValueTask<TValue> GetValueAsync(TContext context, CancellationToken cancellationToken) =>
+        ValueTask.FromResult(GetValue(context));
 }
