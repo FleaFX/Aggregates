3512bc01474ef36b7a95448bcc6ccef3583eb5f7
Wouter Naessens <wouter.naessens@sterima.be>
2026-05-22 22:56:08 +0200

    Support subscribing from the end of a stream

--- src/Aggregates.MSSP/Subscriptions/MsspSubscriptionFactory.cs
+++ src/Aggregates.MSSP/Subscriptions/MsspSubscriptionFactory.cs
@@ -10,12 +10,15 @@ namespace Aggregates.MSSP.Subscriptions;
 public sealed class MsspSubscriptionFactory(IMsspClient client, MsspOptions options) : ISubscriptionFactory {
     /// <inheritdoc />
     public ISubscription Subscribe(ulong? fromPosition, bool startFromEnd, CancellationToken cancellationToken = default) {
-        if (startFromEnd)
-            throw new NotSupportedException("Subscribing from the end of a stream is currently not supported in MSSP.");
+        var from = (fromPosition, startFromEnd) switch {
+            ({} pos, _) => new GlobalPosition(pos),
+            (null, true) => GlobalPosition.End,
+            _ => GlobalPosition.Start
+        };
 
         return new MsspSubscription(
             () => ValueTask.CompletedTask,
-            client.SubscribeAsync(SubscriptionFilter.All, fromPosition is {} value ? new GlobalPosition(value) : GlobalPosition.Start, cancellationToken),
+            client.SubscribeAsync(SubscriptionFilter.All, from, cancellationToken),
             options
         );
     }
